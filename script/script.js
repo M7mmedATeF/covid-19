88afd9fc6e44ed3f3bb5393eba4bce3bac9eae47
M7mmedATeF <91163810+M7mmedATeF@users.noreply.github.com>
2021-09-22 16:04:01 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(() => {
 
     // Variables
@@ -97,71 +96,4 @@ $(() => {
     function mobileNav() {
         $('.mobileNav').fadeToggle();
     }
-=======
-$(() => {
-
-    // Variables
-    const aboutBTNS = [
-        { link: $('.aboutNAV'), place: "#about", classed: ".aboutNAV" },
-        { link: $('header section .left .link') },
-        { link: $('.mobAbout') },
-    ];
-
-    const homeBTNS = [
-        { link: $('.homeNAV'), place: "#home", classed: ".homeNAV" },
-        { link: $('.goUp') },
-        { link: $('.mobHome') },
-    ];
-
-    const sympBTNS = [
-        { link: $('.sympNAV'), place: "#symp", classed: ".sympNAV" },
-        { link: $('.mobSymp') },
-    ];
-    const preventBTNS = [
-        { link: $('.prevNAV'), place: "#prevent", classed: ".prevNAV" },
-    ];
-
-    // Move Between Sections
-    applyFn(aboutBTNS);
-    applyFn(homeBTNS);
-    applyFn(sympBTNS);
-    applyFn(preventBTNS);
-
-    function scrollFN(scrollTo, classed) {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(scrollTo).offset().top
-        }, 100);
-        $('.stickyNav ul li').removeClass('active');
-        $('.homeNAV').removeClass('active');
-        $(classed).addClass('active');
-    }
-
-    function applyFn(arr) {
-        for (var i = 0; i < arr.length; i++) {
-            arr[i].link.click(() => {
-                scrollFN(arr[0].place, arr[0].classed)
-            });
-        }
-    }
-
-    // Sticky Nav
-    $(document).scroll(() => {
-        if ($(window).scrollTop() > 100) {
-            $('nav').addClass('stickyNav');
-            $('.goUp').fadeIn();
-        } else {
-            $('nav').removeClass('stickyNav');
-            $('.goUp').fadeOut();
-        }
-    })
-
-    // Mobile Nav
-    $('#phoneNavClose').click(mobileNav);
-    $('#MNV').click(mobileNav);
-    $('.mobileNav ul li').click(mobileNav);
-
-    function mobileNav() {
-        $('.mobileNav').fadeToggle();
-    }
->>>>>>> 6de624b4bb9f98d687c5dda5d66024f6ba3d0fb0
 })
